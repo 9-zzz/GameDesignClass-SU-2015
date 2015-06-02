3f311c76276cf93097e68650f96eeec49169078e
Shahan Akhter <shahan3.14@gmail.com>
2015-06-02 14:25:35 -0700

    first commit with unity project in and gitignore.

---
+++ myUnityProjects/myRollABall/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if(Input.GetKeyDown("space"))
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+
+        }
+    }
+
+}
